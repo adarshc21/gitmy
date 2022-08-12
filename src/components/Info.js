61635a68fc5e4a4a8915658ba0cd8063d09d08b3
Adarsh Chavhan <adarshchavhan@outlook.com>
2022-08-12 06:33:24 +0530

    initial commit

---
+++ src/components/Info.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Info({ data }) {
+  const {
+    name,
+    login,
+    avatar_url,
+    html_url,
+    company,
+    location,
+    created_at,
+    public_repos,
+    followers,
+    following,
+  } = data;
+
+  return (
+    <SInfo>
+      <Link className="back-btn" to="/">
+        <span className="iconify" data-icon="akar-icons:home"></span>
+      </Link>
+
+      <img src={avatar_url} className="avatar" alt="profile img" />
+      <h2 className="name">{name}</h2>
+      <a href={html_url}>
+        <h3 className="username">@{login}</h3>
+      </a>
+      <ul className="info">
+        {company !== null && (
+          <li>
+            <span className="iconify" data-icon="ic:round-work-outline"></span>
+            <span className="text">{company}</span>
+          </li>
+        )}
+        {location !== null && (
+          <li>
+            <span className="iconify" data-icon="akar-icons:location"></span>
+            <span className="text">{location}</span>
+          </li>
+        )}
+
+        <li>
+          <span className="iconify" data-icon="uil:calender"></span>
+          <span className="text">joined {created_at.slice(0, 10)}</span>
+        </li>
+      </ul>
+
+      <ul className="brief">
+        <li>
+          <h2 className="no">{public_repos}</h2>
+          <small className="text">REPOSITORIES</small>
+        </li>
+        <li>
+          <h2 className="no">{followers}</h2>
+          <small className="text">FOLLOWERS</small>
+        </li>
+        <li>
+          <h2 className="no">{following}</h2>
+          <small className="text">FOLLOWING</small>
+        </li>
+      </ul>
+    </SInfo>
+  );
+}
+
+const SInfo = styled.section`
+  text-align: center;
+  padding: 90px 0px;
+  .back-btn {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    color: ${({ theme }) => theme.baseColor};
+  }
+  .avatar {
+    width: 140px;
+    padding: 7px;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.baseColor};
+  }
+  .name {
+    color: ${({ theme }) => theme.lightColor};
+    margin: 5px;
+    font-weight: 600;
+    letter-spacing: 0px;
+  }
+  .username {
+    color: ${({ theme }) => theme.baseColor};
+    margin: 10px;
+    font-family: ${({ theme }) => theme.font2};
+    font-weight: 500;
+  }
+  .info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    li {
+      color: ${({ theme }) => theme.lightColor};
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+      margin: 5px 10px;
+      span {
+        padding: 5px;
+      }
+    }
+  }
+
+  .brief {
+    color: ${({ theme }) => theme.lightColor};
+    display: flex;
+    justify-content: center;
+    margin: 15px 0;
+    gap: 10px;
+    li {
+      background: ${({ theme }) => theme.darkColor1};
+      padding: 15px 20px;
+      border-radius: 5px;
+      display: flex;
+      flex-direction: column;
+      .no {
+        font-weight: 500;
+      }
+      .text {
+        margin-top: 5px;
+        padding: 5px;
+        font-size: 12px;
+        letter-spacing: 1px;
+      }
+    }
+  }
+
+  @media (max-width: 568px) {
+    padding-bottom: 0px;
+    .info {
+      flex-direction: column;
+    }
+    .brief {
+      transform: scale(0.9);
+      flex-direction: column;
+      align-items: center;
+      li {
+        width: 180px;
+        .text {
+          font-size: 12px;
+        }
+      }
+    }
+  }
+`;
---
+++ src/components/UserPage.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Info from "./Info";
+import Repo from "./Repo";
+import Error from "./Error";
+
+export default function UserPage() {
+  const { param } = useParams();
+  const [Found, setFound] = useState(false);
+  const [NotFound, setNotFound] = useState(false);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    document.title = `Gitmy - ${param}`;
+
+    axios(`https://api.github.com/users/${param}`)
+      .then((response) => {
+        setFound(true);
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        setNotFound(true);
+        console.clear();
+      });
+  }, [param]);
+
+  return (
+    <div>
+      {Found && (
+        <>
+          <Info data={data} />
+          <Repo repos_url={data.repos_url} />
+        </>
+      )}
+
+      {NotFound && <Error />}
+    </div>
+  );
+}
